333e91ef7a4729ca605fcc898ce41b602d3fd593
LucasNietoSilva <nietolucas.lns@gmail.com>
2023-02-22 20:36:45 -0300

    rama con errores

--- inmobiliaria/src/components/header/Header.jsx
+++ inmobiliaria/src/components/header/Header.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import ButtonsHeader from "./Buttons/ButtonsHeader";
 import "./header.css";
+import Nav from "./nav_burger/Nav";
+import NavList from "./nav_list/NavList";
 
 function Header() {
-  return <div className="cont-header">Header</div>;
+  return (
+    <>
+      <header>
+        <div className="image">
+          <img src="./imgs/LogoSenpai_Inm_Blanco.png" alt="logo" />
+        </div>
+        {/* <div className="container-nav">
+          <NavList />
+        </div> */}
+        <ButtonsHeader />
+        <div className="container-nav">
+          <Nav />
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
