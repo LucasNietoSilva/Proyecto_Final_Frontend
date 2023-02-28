3ec90c0b1a8647e9c39217ad3c47cb9988d2f191
LucasNietoSilva <nietolucas.lns@gmail.com>
2023-02-28 19:23:00 -0300

    cambios color iconos footer css

--- inmobiliaria/src/App.js
+++ inmobiliaria/src/App.js
@@ -1,7 +1,6 @@
 import Lista from "./components/Main/Main";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Register from "./components/Registrer/RegistrationForm.jsx"
 import { useEffect, useState } from "react";
 import { getPublicaciones } from "./api/Rule_info";
 
